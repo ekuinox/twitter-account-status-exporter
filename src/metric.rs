151c696a649d2b926e2a6e54144f69e9797f9b75
ekuinox <depkey@me.com>
2022-03-10 14:23:48 +0900

    join_allできるようにした

--- src/metric.rs
+++ src/metric.rs
@@ -1,6 +1,7 @@
 use crate::twitter::{TwitterClient, UserResponse, UserResponseErrorTitle};
 use cached::proc_macro::cached;
 use chrono::Utc;
+use futures::future::join_all;
 
 #[derive(Debug)]
 #[repr(u8)]
@@ -37,20 +38,34 @@ impl From<UserResponse> for AccountStatus {
 
 #[cached(time = 60)]
 pub async fn get_metric(client: TwitterClient, usernames: Vec<String>) -> String {
-    let mut response = String::with_capacity(usernames.len() * 12);
-    let time = Utc::now().timestamp_millis();
-    response += "# HELP twitter twitter_account_status\n";
-    response += "# TYPE twitter summary\n";
-    for username in usernames {
-        let status: AccountStatus = client
+    async fn get_status(username: String, client: &TwitterClient) -> (String, AccountStatus, i64) {
+        let status = client
             .get_user(&username)
             .await
             .map(From::from)
             .unwrap_or_default();
-        response += &format!(
-            "twitter{{account=\"{}\"}} {:?} {}\n",
-            username, status as u8, time
-        );
+        let time = Utc::now().timestamp_millis();
+        (username, status, time)
     }
-    response
+
+    let metrics = join_all(
+        usernames
+            .into_iter()
+            .map(|username| get_status(username, &client)),
+    )
+    .await;
+
+    let metrics = metrics.into_iter().fold(
+        "# HELP twitter twitter_account_status\n# TYPE twitter summary\n".to_string(),
+        |mut acc, (username, status, time)| {
+            let metric = format!(
+                "twitter{{account=\"{}\"}} {:?} {}\n",
+                username, status as u8, time
+            );
+            acc.push_str(&metric);
+            acc
+        },
+    );
+
+    metrics
 }
